f17e2ee58c990c1e95471b45fe177c8a66504a7b
shobhit kumar <shobhit.4454@gmail.com>
2024-02-10 02:17:15 -0500

    added service section

--- src/Component/Review.jsx
+++ src/Component/Review.jsx
@@ -1,100 +1,123 @@
-import React, { useRef, useEffect } from "react";
-import Swiper from "swiper";
-import "swiper/swiper-bundle.css";
-import Btn from "../Common/Btn";
+import React from "react";
+import Slider from "react-slick";
 import Arrow from "../Common/Arrow";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import arrowleft from "../Images/button-arrow-left.svg";
 
-export default function Review() {
-  const swiperContainer = useRef(null);
-  const swiperPrevButton = useRef(null);
-  const swiperNextButton = useRef(null);
+const data = [
+  {
+    name: "Guy Hawkins",
+    img: "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707436800&semt=ais",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor Lorem ipsum ",
+  },
+  {
+    name: "Guy Hawkins",
+    img: "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707436800&semt=ais",
+    review:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor Lorem ipsum ",
+  },
+  {
+    name: "Guy Hawkins",
+    img: "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707436800&semt=ais",
+    review:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor Lorem ipsum ",
+  },
+  {
+    name: "Usha Singh",
+    img: "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707436800&semt=ais",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor Lorem ipsum ",
+  },
+  {
+    name: "Guy Hawkins",
+    img: "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707436800&semt=ais",
+    review:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor Lorem ipsum ",
+  },
+  {
+    name: "Guy Hawkins",
+    img: "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.87170709.1707436800&semt=ais",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor Lorem ipsum ",
+  },
+];
 
-  useEffect(() => {
-    const swiper = new Swiper(swiperContainer.current, {
-      slidesPerView: "auto",
-      spaceBetween: 30,
-      navigation: {
-        prevEl: swiperPrevButton.current,
-        nextEl: swiperNextButton.current,
-      },
-    });
-    return () => {
-      swiper.destroy();
-    };
-  }, []);
+function Review() {
+  const sliderRef = React.useRef();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
+  const goToNext = () => {
+    sliderRef.current.slickNext();
+  };
+
+  const goToPrev = () => {
+    sliderRef.current.slickPrev();
+  };
 
   return (
-    <section className="mx-auto bg-blue">
-      <div className="container mx-auto" ref={swiperContainer}>
-        <div className="swiper-wrapper">
-          <div className="swiper-slide">
-            <div className="h-96 w-96 bg-orange relative rounded-3xl mx-auto">
+    <section className="mx-auto bg-blue py-5">
+      <div className="container m-auto">
+        <div className="mx-auto container flex justify-between items-center my-10">
+          <h1 className="text-h1 font-bold text-yellow w-96 leading-[4.8rem]">
+            Review Of Our Services
+          </h1>
+          <div className="w-[50%]">
+            <p className="text-white ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+              tincidunt, mi ac fermentum congue, diam urna finibus lectus, at
+              Lorem ipsum dolor
+            </p>
+          </div>
+        </div>
+        <Slider {...settings} ref={sliderRef}>
+          {data.map((d, index) => (
+            <div
+              key={index}
+              className="h-[70vh] w-96 flex items-center bg-gradient-to-r from-yellow font-bold to-orange relative rounded-3xl mx-auto"
+            >
+              <div className=" flex justify-center items-center h-[40vh] w-full">
               <img
-                src="https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340"
-                className="object-cover absolute w-full h-full object-center rounded-3xl"
+                src={d.img}
+                className=" object-cover absolute h-40 object-center rounded-full"
                 alt=""
               />
-              <div className="content absolute bottom-0 px-4">
-                <h3 className="text-white text-h3 font-bold leading-[1.9rem] my-6">
-                  Financial Investments
+              </div>
+              <div className="content absolute px-4">
+                <h3 className="text-black text-h3 font-bold text-center leading-[1.9rem] my-6">
+                  {d.name}
                 </h3>
-                <div className="flex my-2">
-                  <div>
-                    <Btn />
-                  </div>
-                  <div className="mx-3">
-                    <Arrow />
-                  </div>
+                <div>
+                  <p className="font-bold text-center">{d.review}</p>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="swiper-slide">
-            <div className="h-96 w-96 bg-orange relative rounded-3xl mx-auto">
-              <img
-                src="https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340"
-                className="object-cover absolute w-full h-full object-center rounded-3xl"
-                alt=""
-              />
-              <div className="content absolute bottom-0 px-4">
-                <h3 className="text-white text-h3 leading-[1.9rem] font-bold my-6">
-                  Budget Savings
-                </h3>
-                <div className="flex my-2">
-                  <div>
-                    <Btn />
-                  </div>
-                  <div className="mx-3">
-                    <Arrow />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="swiper-slide">
-            <div className="h-96 w-96 bg-orange relative rounded-3xl mx-auto">
-              <img
-                src="https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340"
-                className="object-cover absolute w-full h-full object-center rounded-3xl"
-                alt=""
-              />
-              <div className="content absolute bottom-0 px-4 flex flex-col">
-                <h3 className="text-white text-h3 leading-[1.9rem] font-bold my-6">
-                  Financial Investments
-                </h3>
-                <div className="flex my-2 ">
-                  <div>
-                    <Btn />
-                  </div>
-                  <div className="mx-3">
-                    <Arrow />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
+        </Slider>
+        <div className="flex justify-center my-4">
+          <button onClick={goToPrev} className="mr-4">
+            <img
+              src={arrowleft}
+              className="h-11 w-11 bg-yellow rounded-full"
+              alt=""
+            />
+          </button>
+          <button onClick={goToNext}>
+            {" "}
+            <Arrow />
+          </button>
         </div>
       </div>
     </section>
   );
 }
+
+export default Review;
